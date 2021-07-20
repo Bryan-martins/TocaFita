14154174d538e88031763baa803590577a183206
Valderlandia-Macedo <valderlandiamacedo@hotmail.com>
2021-07-20 10:54:49 -0300

    Arquivo teste

---
+++ test.py
@@ -0,0 +1,7 @@
+from NSOQCNM import TocaFita
+
+teste = TocaFita.TocaFita()
+teste.player = "C:\Program Files (x86)\Windows Media Player\wmplayer.exe"
+teste.Tocar(r"C:\Users\Val\Desktop\Musicas\Hozier-Take-Me-To-Church.mp3")
+
+#teste.TocarLista(r"C:\Users\Val\Desktop\Musicas\Hozier-Take-Me-To-Church.mp3","C:\Users\Val\Desktop\Musicas\Konai-Brown-Cidade-Lunar.mp3")
